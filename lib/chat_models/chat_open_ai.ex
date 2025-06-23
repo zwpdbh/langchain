03e8e1522ec2fd7627ee96cd388fafd8ac1f5932
zwpdbh <hyperion_z@outlook.com>
2025-06-23 10:11:45 +0800

    > test

--- lib/chat_models/chat_open_ai.ex
+++ lib/chat_models/chat_open_ai.ex
@@ -324,6 +324,7 @@ defmodule LangChain.ChatModels.ChatOpenAI do
     |> cast(attrs, @create_fields)
     |> common_validation()
     |> apply_action(:insert)
+    |> IO.inspect(lable: "ChatOpenAI -- new")
   end
 
   @doc """
